bbcf6ab9175e1ebdd91b2adf3ef78c1a0b3dba3d
SLiPCoR <chris@slipcor.de>
2016-03-11 00:13:47 +0100

    0.8.24 - fix the override replacement

--- src/main/java/net/slipcor/pvpstats/PSMySQL.java
+++ src/main/java/net/slipcor/pvpstats/PSMySQL.java
@@ -353,13 +353,13 @@ public final class PSMySQL {
                     for (int i = 0; i < lines.size(); i++) {
                         String line = lines.get(i);
 
-                        line.replace("%d", String.valueOf(deaths));
-                        line.replace("%k", String.valueOf(kills));
-                        line.replace("%m", String.valueOf(maxStreak));
-                        line.replace("%n", name);
-                        line.replace("%r", String.valueOf(ratio));
-                        line.replace("%s", String.valueOf(streak));
-                        line.replace("%e", String.valueOf(elo));
+                        line = line.replace("%d", String.valueOf(deaths));
+                        line = line.replace("%k", String.valueOf(kills));
+                        line = line.replace("%m", String.valueOf(maxStreak));
+                        line = line.replace("%n", name);
+                        line = line.replace("%r", String.valueOf(ratio));
+                        line = line.replace("%s", String.valueOf(streak));
+                        line = line.replace("%e", String.valueOf(elo));
 
                         output[i] = line;
                     }
